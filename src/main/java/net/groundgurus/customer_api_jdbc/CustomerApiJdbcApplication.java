39b27e14cdecd0a393c35e890f3eca142f414f30
Philip Mark Gutierrez <sensei_slayer@protonmail.com>
2025-07-15 16:17:11 +0800

    init

---
+++ src/main/java/net/groundgurus/customer_api_jdbc/CustomerApiJdbcApplication.java
@@ -0,0 +1,13 @@
+package net.groundgurus.customer_api_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiJdbcApplication.class, args);
+	}
+
+}
